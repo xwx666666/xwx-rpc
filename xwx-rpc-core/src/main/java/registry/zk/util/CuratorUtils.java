7a389e44bc379b5ac626cc08ce28eb6cdd32b7b4
secur1ty <1127366326@qq.com>
2022-08-22 17:43:13 +0800

    some updates about CuratorUtils

--- xwx-rpc-core/src/main/java/registry/zk/util/CuratorUtils.java
+++ xwx-rpc-core/src/main/java/registry/zk/util/CuratorUtils.java
@@ -5,6 +5,9 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 
@@ -21,10 +24,11 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class CuratorUtils {
 
-    private static String ZK_ROOT="/xwx-rpc/";
+    private static String ZK_ROOT="/xwx-rpc";
     private static CuratorFramework zkClient;
     private static Set<String> REGISTERED_PATH_SET= ConcurrentHashMap.newKeySet();
-    private static Map<String,String> SERVICE_ADDRESS_MAP=new ConcurrentHashMap<>();
+    //key: serviceName  value: address
+    private static Map<String,List<String>> SERVICE_ADDRESS_MAP=new ConcurrentHashMap<>();
     /**
      * get connection to zookeeper
      * @return
@@ -50,7 +54,7 @@ public class CuratorUtils {
      }
 
     /**
-     *
+     * add a address about a service
      * @param zkClient
      * @param path /xwx-rpc/HelloService/127.0.0.1:2181
      */
@@ -66,12 +70,44 @@ public class CuratorUtils {
          }
      }
 
+    /**
+     * get all the address about a service
+     * @param zkClient
+     * @param serviceName
+     * @return
+     */
      public static List<String> getChildNodes(CuratorFramework zkClient,String serviceName){
-            if(SERVICE_ADDRESS_MAP.containsKey(serviceName)){
 
+            if(SERVICE_ADDRESS_MAP.containsKey(serviceName)){
+                  return SERVICE_ADDRESS_MAP.get(serviceName)
             }
-            return null;
+            String path=ZK_ROOT+"/"+serviceName;
+         List<String> list=null;
+         try {
+             list= zkClient.getChildren().forPath(path);
+             SERVICE_ADDRESS_MAP.put(serviceName,list);
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+         return list;
      }
+
+    /**
+     * if there are some changes about the children(some service address) of service node,listener will do some thing below
+     */
+    private static void registerWatcher(String serviceName,CuratorFramework zkClient) throws Exception {
+        String path=ZK_ROOT + "/" + serviceName;
+        PathChildrenCache pathChildrenCache = new PathChildrenCache(zkClient, path, true);
+        pathChildrenCache.getListenable().addListener(new PathChildrenCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework curatorFramework, PathChildrenCacheEvent pathChildrenCacheEvent) throws Exception {
+                //update the map
+                List<String> list= zkClient.getChildren().forPath(path);
+                SERVICE_ADDRESS_MAP.put(serviceName,list);
+            }
+        });
+        pathChildrenCache.start();
+    }
 
 
 
